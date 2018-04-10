665385335b5f6f30545a1353ac4f263122016144
魏帅 <1169774483@qq.comm>
2018-04-10 18:06:17 +0800

    第二次添加文件

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@function px2rem($px) {
+    $rem: 37.5;
+    @return ($px / $rem)+rem;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: px2rem(60);
+  font-size: px2rem(16);
 }
 </style>
